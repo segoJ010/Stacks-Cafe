0fdca0191a9882549561f8084effd47f147c3462
carse <carse@192.168.2.9>
2020-06-27 20:31:10 -0400

    Stacks-Cafe master update on toString() method

--- DrinkItem.java
+++ DrinkItem.java
@@ -1,10 +1,10 @@
 package StacksCafe;
 import java.text.NumberFormat;
 
-public class DrinkItem extends LunchItem{
-  private int size;
+public class DrinkItem extends LunchItem{ //b extends a = b made a bigger and fatter
+  private int size; //private because we dont want the contents of the memory address fucked up
   
-  public DrinkItem(String name, double cost, int calories, int size) {
+  public DrinkItem(String name, double cost, int calories, int size) { //method w/ 4 parameters
       super(name, cost, calories);
       this.size = size;
   }
@@ -15,7 +15,7 @@ public class DrinkItem extends LunchItem{
 @Override
   public String toString() {
       NumberFormat formatter = NumberFormat.getCurrencyInstance();
-      return size + " ounces of " + getName() + " (" + getCalories() + " calories) - "
+      return size + " great ounces of " + getName() + " (" + getCalories() + " calories) - "
                   + formatter.format(getCost());
     }
 }
